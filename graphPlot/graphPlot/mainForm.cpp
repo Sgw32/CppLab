999781269057e523d5e877881ad0b1434b4aaaa8
PHL <sgw32@yandex.ru>
2017-04-18 00:46:15 +0300

    graphPlot folder, C++/CLI

---
+++ graphPlot/graphPlot/mainForm.cpp
@@ -0,0 +1,12 @@
+#include "mainForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+void Main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	graphPlot::mainForm form;
+	Application::Run(%form);
+}
---
+++ graphPlot/graphPlot/mainForm.h
@@ -0,0 +1,212 @@
+#pragma once
+
+namespace graphPlot {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for mainForm
+	/// </summary>
+	public ref class mainForm : public System::Windows::Forms::Form
+	{
+	public:
+		Bitmap^ DrawArea;
+		mainForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+			
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~mainForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::PictureBox^  pictureBox1;
+	protected:
+	private: System::Windows::Forms::GroupBox^  groupBox1;
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::Label^  label4;
+	private: System::Windows::Forms::TextBox^  textBox4;
+	private: System::Windows::Forms::Label^  label3;
+	private: System::Windows::Forms::TextBox^  textBox3;
+	private: System::Windows::Forms::Label^  label2;
+	private: System::Windows::Forms::TextBox^  textBox2;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::TextBox^  textBox1;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
+			this->groupBox1->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Location = System::Drawing::Point(12, 12);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(754, 462);
+			this->pictureBox1->TabIndex = 0;
+			this->pictureBox1->TabStop = false;
+			// 
+			// groupBox1
+			// 
+			this->groupBox1->Anchor = static_cast<System::Windows::Forms::AnchorStyles>((System::Windows::Forms::AnchorStyles::Top | System::Windows::Forms::AnchorStyles::Right));
+			this->groupBox1->Controls->Add(this->button1);
+			this->groupBox1->Controls->Add(this->label4);
+			this->groupBox1->Controls->Add(this->textBox4);
+			this->groupBox1->Controls->Add(this->label3);
+			this->groupBox1->Controls->Add(this->textBox3);
+			this->groupBox1->Controls->Add(this->label2);
+			this->groupBox1->Controls->Add(this->textBox2);
+			this->groupBox1->Controls->Add(this->label1);
+			this->groupBox1->Controls->Add(this->textBox1);
+			this->groupBox1->Location = System::Drawing::Point(786, 12);
+			this->groupBox1->Name = L"groupBox1";
+			this->groupBox1->Size = System::Drawing::Size(247, 152);
+			this->groupBox1->TabIndex = 1;
+			this->groupBox1->TabStop = false;
+			this->groupBox1->Text = L"Настройки";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(75, 32);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(160, 20);
+			this->textBox1->TabIndex = 0;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(13, 35);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(56, 13);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"Функция:";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(26, 61);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(30, 13);
+			this->label2->TabIndex = 3;
+			this->label2->Text = L"Xmin";
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(75, 58);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(56, 20);
+			this->textBox2->TabIndex = 2;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(137, 61);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(33, 13);
+			this->label3->TabIndex = 5;
+			this->label3->Text = L"Xmax";
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(178, 58);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(57, 20);
+			this->textBox3->TabIndex = 4;
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(26, 87);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(30, 13);
+			this->label4->TabIndex = 7;
+			this->label4->Text = L"Шаг:";
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(75, 84);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(56, 20);
+			this->textBox4->TabIndex = 6;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(160, 111);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 8;
+			this->button1->Text = L"Построить";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// mainForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1041, 488);
+			this->Controls->Add(this->groupBox1);
+			this->Controls->Add(this->pictureBox1);
+			this->Name = L"mainForm";
+			this->Text = L"Графопостроитель";
+			this->Load += gcnew System::EventHandler(this, &mainForm::mainForm_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
+			this->groupBox1->ResumeLayout(false);
+			this->groupBox1->PerformLayout();
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void mainForm_Load(System::Object^  sender, System::EventArgs^  e) {
+
+				 DrawArea = gcnew Bitmap(pictureBox1->Size.Width, pictureBox1->Size.Height);
+				 pictureBox1->Image = DrawArea;
+
+				 Graphics^ g;
+				 g = Graphics::FromImage(DrawArea);
+
+				 SolidBrush^ mybrush = gcnew SolidBrush(Color::White);
+				 
+				 g->FillRectangle(mybrush, Rectangle(0, 0, pictureBox1->Height, pictureBox1->Width));
+				 pictureBox1->Image = DrawArea;
+				 delete g;
+	}
+};
+}
